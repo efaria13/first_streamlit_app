37d062b4da90e1bc690dfa94b63ef0c6d112040f
efaria13 <67074431+efaria13@users.noreply.github.com>
2023-07-20 23:21:04 -0300

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -22,4 +22,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) #just write data on screen
+
+# Normalizing data with pandas
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Creating dataframe
+streamlit.dataframe(fruityvice_normalized)
